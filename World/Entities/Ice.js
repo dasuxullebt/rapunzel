f87c30b494513119757ce4ff84522b464221eddf
Tobias Udtke <tobias.udtke@gmail.com>
2017-01-13 15:02:34 +0100

    Ice should now properly move Entities and should now longer move the player through a box

--- World/Entities/Ice.js
+++ World/Entities/Ice.js
@@ -6,53 +6,42 @@
 var Ice=function(world,tile)
 {
     Entity.call(this,world,tile,'ice');
+    
     this.zIndex=-1;
     
     this.update=function(){};
-    this.isPlayerAllowedToEnterFrom = function(player, direction) { return true; };
+    
+    this.isPlayerAllowedToEnterFrom = function(player, direction) 
+    { 
+        return true; 
+    };
+    
     this.onEnter = function(player, direction)
-    {
-		console.log(player+"has entered");
-
-		for (var x = 0; x < world.size.x; x++) 
-			for (var y = 0; y < world.size.y; y++) 
-				for (var z = 0; z < world.grid[x][y].entities.length; z++) 
-					if (world.grid[x][y].entities[z] == player)
-							world.grid[x][y].entities.splice(z,1);
-		
+    {   
+        for (var i = 0; i < this.tile.entities.length; i++)
+        {
+            if (this.tile.entities[i] != this)
+                if(this.tile.entities[i].onEnter)
+                    this.tile.entities[i].onEnter(player,direction);
+        }
+        
         if(!(this.tile.getNeighbour(direction)==null))
         {
             if(this.tile.getNeighbour(direction).isPlayerAllowedToEnterFrom(direction))//=if(keine der Entities ist eine Große Box ist)
             {
-				this.tile.playerLeavesTile(player,direction);
-				player.tile = this.tile.getNeighbour(direction);
-				player.tile.playerEntersTile(player,direction);
-
-				for (var x = 0; x < world.size.x; x++) 
-					for (var y = 0; y < world.size.y; y++) 
-						for (var z = 0; z < world.grid[x][y].entities.length; z++) 
-							if (world.grid[x][y].entities[z] == player)
-									world.grid[x][y].entities.splice(z,1);
-				player.tile.entities.push(player);	
+                player.moveToTile(this.tile.getNeighbour(direction));
             }
-			else
-			{
-				player.tile = this.tile;
+            else
+            {
+                  
+            }
+        }               
 
-				player.tile.entities.push(player);	
-			}
-		}		
-	
    
     };
 
-	this.onLeave = function(player, direction)
-	{
-		for (var x = 0; x < world.size.x; x++) 
-			for (var y = 0; y < world.size.y; y++) 
-				for (var z = 0; z < world.grid[x][y].entities.length; z++) 
-					if (world.grid[x][y].entities[z] == player)
-							world.grid[x][y].entities.splice(z,1);
-		player.tile.entities.push(player);	
-	}
+        this.onLeave = function(player, direction)
+        {
+            
+        }
 };
