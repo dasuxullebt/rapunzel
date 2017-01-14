c4e5272f60ef91e3ceffd90b02137b7ce057639f
Tobias Udtke <tobias.udtke@gmail.com>
2017-01-14 03:59:50 +0100

    Directed Portal now creates a Player Dummy, that is opague and plays the first Player move animation, while the real Player plays the Animation starting from the partner Portal

--- World/Entities/DirectedPortal.js
+++ World/Entities/DirectedPortal.js
@@ -1,27 +1,31 @@
 var DirectedPortal = function (world, tile) {
     Entity.call(this, world, tile, 'DirectedPortal');
     //this.isEnabled = true;
-	this.isActivated = true;
+        this.isActivated = true;
     
     this.zIndex = -2;
     
     this.partner = this;
-	
-	// TODO Animation
-    
-	this.onEnter = function(player, direction)
-	{
-		if (this.partner.tile.getNeighbour(direction).isPlayerAllowedToEnterFrom(player, direction))
-		{			
-			player.tile.playerLeavesTile(player, direction);
-			player.tile = this.partner.tile.getNeighbour(direction);
-			player.tile.playerEntersTile(player, direction);
-			
-			//TODO queue second animation
-		}
-	};
-	
-	this.isPlayerAllowedToEnterFrom = function(player, direction)
+        
+    // TODO Animation
+
+    this.onEnter = function(player, direction)
+    {
+        if (this.isActivated && this.partner.isActivated && this.partner.tile.getNeighbour(direction).isPlayerAllowedToEnterFrom(player, direction))
+        {                       
+            Ghost = new PlayerDummy(world, this.tile.getNeighbour(-player.direction));
+            Ghost.direction = player.direction;
+            //Ghost.animation.inAnimation = true;
+            Ghost.animation.startAnimation(Ghost.direction);
+            
+            player.animation.position = this.partner.tile.position;
+            player.moveToTile(this.partner.tile.getNeighbour(direction));
+            
+            //TODO queue second animation
+        }
+    };
+        
+    this.isPlayerAllowedToEnterFrom = function(player, direction)
     {
         return this.partner.tile.getNeighbour(direction).isPlayerAllowedToEnterFrom(player, direction);
     };
